b7e0470a089df8b45bcef95fb4066de25763c173
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-10-14 09:54:43 +0200

    .

--- src/main/java/PrvyPolrok/Tyden41/Cykly.java
+++ src/main/java/PrvyPolrok/Tyden41/Cykly.java
@@ -4,12 +4,34 @@ public class Cykly {
 
     public static void main(String[] args) {
 
-        int[] polecisel = new int[]{1,2,3,4,5,6,7,8,9,10};
-        int nultyindex =polecisel[0];
+        cisla3();
+        System.out.println(" ");
+
+
+    }
+    static void cisla () {
+        int[] polecisel = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int nultyindex = polecisel[0];
         System.out.println(polecisel.length);
-        for (int i =0;i<polecisel.length;i++)
-        {
+        for (int i = 0; i < polecisel.length; i++) {
             System.out.println(polecisel[i]);
         }
+    }
+
+    static void cisla2() {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+    static void cisla3(){
+        int v =0;
+        for (int i = 1; i <= 10; i++) {
+            v=i+v;
+        }
+        System.out.println(v);
+
     }
 }
