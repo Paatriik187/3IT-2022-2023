0b6672ee84bbfc9f7f0da2b7e7ffa05181462294
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-10-22 15:35:16 +0200

    .

---
+++ src/main/java/PrvyPolrok/Tyden42/Uloha.java
@@ -0,0 +1,49 @@
+package PrvyPolrok.Tyden42;
+
+public class Uloha {
+    static int rok = 1975;
+    public static void main(String[] args) {
+        prestupnyrok(rok);
+        System.out.println("-----------");
+        parnyrok(rok);
+
+    }
+
+    static void prestupnyrok(int rok){
+
+        boolean prestupny = false;
+        if (rok % 4 == 0){
+            if (rok % 100 == 0){
+                if (rok % 400 == 0){
+                    prestupny = true;
+                }
+                else{
+                    prestupny = false;
+                }
+            }
+            else{
+                prestupny = true;
+            }
+        }
+        else{
+            prestupny = false;
+        }
+        if (prestupny==true){
+            System.out.println("Rok "+rok+" je prestupny");
+        }
+        else{
+            System.out.println("Rok "+rok+" nie je prestupny");
+        }
+
+    }
+
+    static void parnyrok(int rok){
+
+        if(rok % 2==0){
+            System.out.println("Rok "+rok+" je parny");
+        }
+        else{
+            System.out.println("Rok "+rok+" je neparny");
+        }
+    }
+}
