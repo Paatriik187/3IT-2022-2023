3109cd68c2c8c356a592c9c962fb0128951da6b8
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-10-27 10:20:34 +0200

    .

--- src/main/java/PrvyPolrok/Tyden42/vetvenieKodu.java
+++ src/main/java/PrvyPolrok/Tyden42/vetvenieKodu.java
@@ -1,10 +1,14 @@
 package PrvyPolrok.Tyden42;
 
+import javax.swing.text.View;
+
 public class vetvenieKodu {
 
     public static void main(String[] args) {
 
         Mojemeno("Patrik Smolicek");
+        lenif(true,"Patrik Smolicek");
+        ifelse(false,"Patrik Smolicek");
     }
     public static void Mojemeno(String meno){
 
@@ -15,5 +19,26 @@ public class vetvenieKodu {
             System.out.println("Moje meno je kratsie ako 10 znakov");
         }
 
+    }
+    static void lenif(boolean vieJavu, String meno){
+
+        if (vieJavu){
+
+            meno = meno + " vie Javu";
+        }
+        meno = meno + " vie matematiku";
+
+        System.out.println(meno);
+    }
+    static void ifelse(boolean vieJavu, String meno){
+
+        if (vieJavu){
+
+            meno = meno + " vie Javu";
+        }
+        else {
+            meno = meno + " vie matematiku";
+        }
+        System.out.println(meno);
     }
 }
