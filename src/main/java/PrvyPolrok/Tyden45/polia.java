ef61760d51ae253370fde1f9b196e44ecd8bef56
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-11-11 11:14:31 +0100

    .

--- src/main/java/PrvyPolrok/Tyden45/polia.java
+++ src/main/java/PrvyPolrok/Tyden45/polia.java
@@ -18,7 +18,11 @@ public class polia {
         char[] polesedem=new char[5];
         String[] poleosem=new String[5];
         int[] cisla =new int[] {69,42,0,55,485,333,1598,5,444444444,-25};
+         int vysledok1 = Arrays.binarySearch(cisla,55);
+         System.out.println(vysledok1);
         Arrays.sort(cisla);
+        int vysledok2 = Arrays.binarySearch(cisla,55);
+        System.out.println(vysledok2);
         System.out.println(Arrays.toString(cisla));
         System.out.println(cisla.toString());
     }
