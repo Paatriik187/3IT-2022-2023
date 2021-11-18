48041eacf1e9dbd2bb4bfa598a5e74cb97b7e23c
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-11-18 07:56:10 +0100

    .

--- src/main/java/PrvyPolrok/Tyden45/polia.java
+++ src/main/java/PrvyPolrok/Tyden45/polia.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class polia {
 
     public static void main(String[] args) {
-        osemPoli();
-    }
+    osemPoli();
+}
     static void osemPoli(){
 
         int[] pole =new int[5];
@@ -18,8 +18,8 @@ public class polia {
         char[] polesedem=new char[5];
         String[] poleosem=new String[5];
         int[] cisla =new int[] {69,42,0,55,485,333,1598,5,444444444,-25};
-         int vysledok1 = Arrays.binarySearch(cisla,55);
-         System.out.println(vysledok1);
+        int vysledok1 = Arrays.binarySearch(cisla,55);
+        System.out.println(vysledok1);
         Arrays.sort(cisla);
         int vysledok2 = Arrays.binarySearch(cisla,55);
         System.out.println(vysledok2);
