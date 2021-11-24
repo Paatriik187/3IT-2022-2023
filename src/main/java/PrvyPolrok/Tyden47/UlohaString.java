e0e4bb954accb3e108c3c569200429963221d806
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-11-24 10:30:21 +0100

    .

---
+++ src/main/java/PrvyPolrok/Tyden47/UlohaString.java
@@ -0,0 +1,28 @@
+package PrvyPolrok.Tyden47;
+
+public class UlohaString {
+
+
+    public static void main(String[] args) {
+        jePalindrom("TomasJeFrajer");
+    }
+
+    static boolean jePalindrom (String KontrolneSlovo){
+
+        for(int x = 0;x<KontrolneSlovo.length();x++){
+           // System.out.println(KontrolneSlovo.charAt(x));
+        }
+        String spolu="";
+
+
+        for(int x =KontrolneSlovo.length()-1;x>=0;x--){
+           // System.out.println(KontrolneSlovo.charAt(x));
+            spolu = spolu+KontrolneSlovo.charAt(x);
+
+        }
+        System.out.println(spolu);
+        return false;
+    }
+}
+
+
