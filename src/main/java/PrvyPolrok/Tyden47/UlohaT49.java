3a194349584dad7192b9cf94352896d92c338e25
Patrik Smolicek <patrik.smolicek@sosholic.sk>
2021-12-02 10:32:14 +0100

    .

---
+++ src/main/java/PrvyPolrok/Tyden47/UlohaT49.java
@@ -0,0 +1,13 @@
+package PrvyPolrok.Tyden47;
+
+public class UlohaT49 {
+
+    public static void main(String[] args) {
+        System.out.println(vratmicislo(new int[]{4,3,5,6,7,4,2,}));
+
+    }
+
+    static int vratmicislo(int[] cisla){
+        return cisla.length;
+    }
+}
--- src/main/java/PrvyPolrok/Tyden47/dniVTyzdni.java
+++ src/main/java/PrvyPolrok/Tyden47/dniVTyzdni.java
@@ -3,9 +3,9 @@ package PrvyPolrok.Tyden47;
 public class dniVTyzdni {
     public static void main(String[] args) {
         System.out.println("Den v tyzdni je "+dniVTyzdni(1));
+        dniVTyzdnii(8);
     }
     static String dniVTyzdni(int x){
-
         switch (x) {
             case 1:
             return "pondelok";
@@ -25,5 +25,34 @@ public class dniVTyzdni {
             return "neplatny den";
         }
 
+    }
+    static void dniVTyzdnii(int y){
+        String den="" ;
+        switch (y) {
+            case 1:
+               den = "pondelok";
+               break;
+            case 2:
+                den = "utorok";
+                break;
+            case 3:
+                den = "streda";
+                break;
+            case 4:
+                den = "stvrtok";
+                break;
+            case 5:
+                den = "piatok";
+                break;
+            case 6:
+                den = "sobota";
+                break;
+            case 7:
+                den = "nedela";
+                break;
+            default:
+                den = "neplatny den";
+        }
+        System.out.println(den);
     }
 }
